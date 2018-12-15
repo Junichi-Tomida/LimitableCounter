68c573b2a39951088a28e61aa450ad9bc81b05cd
冨田純一 <tomitajunichi@tomitajunichinomacbook-pro.local>
2018-12-15 19:45:20 +0900

    My first Xcode Commit!

--- LimitableCounter/ViewController.swift
+++ LimitableCounter/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var CountLabel: UILabel!
+    @IBOutlet weak var LimitLabel: UILabel!
+    @IBOutlet weak var LimitField: UITextField!
+    var count:Int = 0
+    var limit:Int! = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func CountButton(_ sender: Any) {
+        if(count != limit!){
+        count = count + 1
+        CountLabel.text = count.description
+        }
+    }
+    
+    @IBAction func LimitBiutton(_ sender: Any) {
+        LimitLabel.text = LimitField.text
+        limit = Int(LimitField.text!)
+        
+    }
 }
 
