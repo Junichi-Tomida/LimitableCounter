49a9ad2daa7680ef062211731348887f9001f47b
冨田純一 <tomitajunichi@tomitajunichinomacbook-pro.local>
2018-12-15 19:31:12 +0900

    Initial Commit

---
+++ LimitableCounter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LimitableCounter
+//
+//  Created by 冨田純一 on 2018/12/15.
+//  Copyright © 2018 infoprax. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
